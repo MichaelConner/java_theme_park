ad2cb33502e02e98a16c6d7672fd3408d93d6a12
Michael Conner <michaelconner@hotmail.co.uk>
2019-02-19 18:13:01 +0000

    mvp 1 completed - visitor class added

---
+++ src/main/java/Visitor.java
@@ -0,0 +1,36 @@
+public class Visitor {
+
+    private int age;
+    private double height;
+    private double wallet;
+
+    public Visitor(int age, double height, double wallet){
+        this.age = age;
+        this.height = height;
+        this.wallet = wallet;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(double wallet) {
+        this.wallet = wallet;
+    }
+}
---
+++ src/test/java/VisitorTest.java
@@ -0,0 +1,54 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class VisitorTest {
+    Visitor visitor;
+
+    @Before
+    public void before(){
+        visitor = new Visitor(14, 1.52, 10.00);
+    }
+
+
+    @Test
+    public void hasAge(){
+        assertEquals(14, visitor.getAge());
+    }
+
+    @Test
+    public void hasHeight(){
+        assertEquals(1.52, visitor.getHeight(),0.00);
+    }
+
+    @Test
+    public void hasWallet(){
+        assertEquals(10.00, visitor.getWallet(),0.00);
+    }
+
+    @Test
+    public void canSetAge(){
+        visitor.setAge(16);
+        assertEquals(16, visitor.getAge());
+    }
+
+    @Test
+    public void canSetHeight(){
+        visitor.setHeight(1.46);
+        assertEquals(1.46, visitor.getHeight(), 0.00);
+    }
+
+    @Test
+    public void canSetWallet(){
+        visitor.setWallet(11.52);
+        assertEquals(11.52, visitor.getWallet(), 0.00);
+    }
+
+
+
+
+
+
+}
+
